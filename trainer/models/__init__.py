d11d0f08a75409e888fa156373def016a703bfcf
LGoGoGo <19931299891@163.com>
2024-05-12 17:36:14 +1200

    0509commit

--- trainer/models/__init__.py
+++ trainer/models/__init__.py
@@ -2,6 +2,6 @@ from .clipzeroshot import CLIPZeroShot  # noqa
 from .cliplinearprobe import CLIPLinearProbe  # noqa
 from .coop import CoOp  # noqa
 from .cocoop import CoCoOp  # noqa
-from .clipadapter import CLIPAdapter  # noqa
+from .clipadapterold import CLIPAdapter  # noqa
 from .rise import RISE  # noqa
 from .nkd import NKD  # noqa
